0801fa36e7b26c5246dc8bc32df9bdd94ddecd6d
ZeroOne-Labs <60297110+ZeroOne-Labs@users.noreply.github.com>
2020-01-25 22:50:14 +0530

    Script upload

---
+++ tutorial/Day 1/datatypes.py
@@ -0,0 +1,10 @@
+print(type(23))
+
+print(type(34.5))
+
+print(type("Hello world"))
+
+print(type(True))
+
+
+
---
+++ tutorial/Day 1/variable.py
@@ -0,0 +1,29 @@
+student_name = "Shiva"
+tamil_mark = 90
+maths_mark  = 90
+total_marks = tamil_mark + maths_mark
+
+print("Student Marksheet")
+print(student_name)
+print(tamil_mark,maths_mark,total_marks)
+
+tamil_mark = 95
+total_marks = tamil_mark + maths_mark
+
+print("Updated Student Marksheet")
+print(student_name)
+print(tamil_mark,maths_mark,total_marks)
+
+
+
+
+
+
+
+
+# print("\nUpdated Student Marksheet")
+# print("\nStduent Name: " + student_name)
+# print("\nMaths mark: " + str(maths_mark))
+# print("\nTamil mark: " + str(tamil_mark))
+# print("\nTotal mark: " + str(total_marks))
+
